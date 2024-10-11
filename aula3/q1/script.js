e205ce7d9f2eb6e6b40aaf771e93296dcc7caa88
marcostsi <lira.santos@academico.ifpb.edu.br>
2024-10-11 20:19:06 +0000

    atividade

---
+++ aula3/q1/script.js
@@ -0,0 +1,16 @@
+let n1 = parseInt("Digite o primeiro numero:")
+let operador = parseInt("Digite o operador")
+let n2 = parseInt("Digite o segundo numero:")
+
+if (operador == '+'){
+    console.log(n1 + n2)
+}
+if (operador == '-'){
+    console.log(n1 - n2)
+}
+if (operador == '*'){
+    console.log(n1 * n2)
+}
+if (operador == '/'){
+    console.log(n1 / n2)
+}
