cca5ac8780a65c14f7709034b24eef6a379d9fe6
marcostsi <lira.santos@academico.ifpb.edu.br>
2024-10-12 17:10:38 +0000

    fazendo a primeira questão

---
+++ aula2/q1/script.js
@@ -0,0 +1,23 @@
+function calculadora() {
+    let n1 = parseInt(prompt("Digite o primeiro numero: "))
+let operador = prompt("Digite o operador: ")
+let n2 = parseInt(prompt("Digite o segundo numero: "))
+
+
+if (operador == "+"){
+    console.log(n1 + n2)
+    alert("O resultado é: " + (n1 + n2))
+}
+if (operador == "-"){
+    console.log(n1 - n2)
+    alert("O resultado é: " + (n1 - n2))
+}
+if (operador == "*"){
+    console.log(n1 * n2)
+    alert("O resultado é: " + (n1 * n2))
+}
+if (operador == "/"){
+    console.log(n1 / n2)
+    alert("O resultado é: " + (n1 / n2))
+}
+}
